0d9d3b3dd83e532dacc5c70c19b032fceb53b19c
Thomas Macrina <tmacrina@princeton.edu>
2020-07-13 14:37:05 +0000

    Correct get_field_weights to get_vote_weights
    * Rename get_field_weights to get_vote_weights
    * Make function a method of DisplacementField
    * Return softmin numerator & denominator
    * Break out helper functions, get_vote_shape & get_vote_subset

--- torchfields/voting.py
+++ torchfields/voting.py
@@ -3,7 +3,7 @@ import torch.nn.functional as F
 
 
 ############################
-# Vector voting implemtation
+# Vector vote implemtation
 ############################
 
 def gaussian_blur(self, sigma=1, kernel_size=5):
@@ -27,6 +27,73 @@ def gaussian_blur(self, sigma=1, kernel_size=5):
     kernel = kernel.expand(2, 1, *kernel.shape)
     return F.conv2d(padded, weight=kernel, groups=2)
 
+def get_vote_shape(self):
+    """Consistently split shape into N fields & shape of field
+    """
+    n, _, *shape = self.shape
+    return n, shape
+
+def get_vote_subsets(self):
+    """Compute list of majority subsets to use in vote
+    """
+    n, _ = self.get_vote_shape()
+    m = (n + 1) // 2  # smallest number that constututes a majority
+    from itertools import combinations
+    mtuples = list(combinations(range(n), m))
+    return mtuples
+
+def get_vote_weights(self, softmin_temp=1, blur_sigma=1):
+    """Calculate softmin weights for batch of displacement fields, indicating 
+    which fields should be considered consensus.
+
+    Args:
+        self: DisplacementField of shape (N, 2, H, W)
+        softmin_temp (float): temperature of softmin to use
+        blur_sigma (float): std dev of the Gaussian kernel by which to blur
+            the softmin inputs. Note that the outputs are not blurred.
+            None or 0 means no blurring.
+
+    Returns:
+        softmin numerator (torch.Tensor): (N, 1, H, W)
+        softmin denominator (torch. Tensor): (N, 1, H, W)
+    """
+    from itertools import combinations
+    if self.ndimension() != 4:
+        raise ValueError('Vector vote is only implemented on '
+                         'displacement fields with 4 dimensions. '
+                         'The input has {}.'.format(self.ndimension()))
+    n, shape = self.get_vote_shape()
+    if n == 1:
+        return self 
+    elif n % 2 == 0:
+        raise ValueError('Cannot vetor vote on an even number of '
+                         'displacement fields: {}'.format(n))
+    blurred = self.gaussian_blur(sigma=blur_sigma) if blur_sigma else self
+    mtuples = self.get_vote_subsets()
+
+    # compute distances for all pairs of fields
+    dists = torch.zeros((n, n, *shape)).to(device=blurred.device)
+    for i in range(n):
+        for j in range(i):
+            dists[i, j] = dists[j, i] \
+                = blurred[i].distance(blurred[j])
+
+    # compute mean distance for all majority tuples
+    mtuple_avg = []
+    for mtuple in mtuples:
+        delta = torch.stack([
+            dists[i, j] for i, j in combinations(mtuple, 2)
+        ]).mean(dim=0)
+        mtuple_avg.append(delta)
+    mavg = torch.stack(mtuple_avg)
+
+    # compute weights for mtuples: smaller mean distance -> higher weight
+    # computing softmin explicitly for access to numerator & denominator
+    # equivalent to:
+    # (-mavg/softmin_temp).softmax(dim=0) == weights_num / weight_den
+    weights_num = torch.exp(-mavg/softmin_temp)
+    weights_den = weights_num.sum(dim=0, keepdim=True)
+    return weights_num, weights_den
 
 def vote(self, softmin_temp=1, blur_sigma=1):
     """Produce a single, consensus displacement field from a batch of
@@ -46,66 +113,19 @@ def vote(self, softmin_temp=1, blur_sigma=1):
 
     Returns:
         DisplacementField of shape (1, 2, H, W) containing the vector
-        voting result
+        vote result
     """
-    field_weights = get_field_weights(self, 
-                                      softmin_temp=softmin_temp,
-                                      blur_sigma=blur_sigma)
-    partition = field_weights.sum(dim=0, keepdim=True)
-    field_weights = field_weights / partition
-    return (self * field_weights.unsqueeze(-3)).sum(dim=0, keepdim=True)
-
-def get_field_weights(field, softmin_temp=1, blur_sigma=1):
-    """Calculate softmin weights for set of fields, indicating which fields
-    should be considered consensus.
-
-    Args:
-        field: DisplacementField of shape (N, 2, H, W)
-        softmin_temp (float): temperature of softmin to use
-        blur_sigma (float): std dev of the Gaussian kernel by which to blur
-            the softmin inputs. Note that the outputs are not blurred.
-            None or 0 means no blurring.
-
-    Returns:
-        torch.Tensor of shape (N, 1, H, W) containing the field weights 
-    """
-    from itertools import combinations
-    if field.ndimension() != 4:
-        raise ValueError('Vector voting is only implemented on '
-                         'displacement fields with 4 dimensions. '
-                         'The input has {}.'.format(field.ndimension()))
-    n, _two_, *shape = field.shape
-    if n == 1:
-        return field
-    elif n % 2 == 0:
-        raise ValueError('Cannot vetor vote on an even number of '
-                         'displacement fields: {}'.format(n))
-    m = (n + 1) // 2  # smallest number that constututes a majority
-    blurred = field.gaussian_blur(sigma=blur_sigma) if blur_sigma else field
-
-    # compute distances for all pairs of fields
-    dists = torch.zeros((n, n, *shape)).to(device=blurred.device)
-    for i in range(n):
-        for j in range(i):
-            dists[i, j] = dists[j, i] \
-                = blurred[i].distance(blurred[j])
-
-    # compute mean distance for all m-tuples
-    mtuples = list(combinations(range(n), m))
-    mtuple_avg = []
-    for mtuple in mtuples:
-        delta = torch.stack([
-            dists[i, j] for i, j in combinations(mtuple, 2)
-        ]).mean(dim=0)
-        mtuple_avg.append(delta)
-    mavg = torch.stack(mtuple_avg)
-
-    # compute weights for mtuples: smaller mean distance -> higher weight
-    mt_weights = (-mavg/softmin_temp).softmax(dim=0)
-
+    n, shape = self.get_vote_shape()
+    mtuples = self.get_vote_subsets()
+    mt_num , mt_den = self.get_vote_weights(softmin_temp=softmin_temp,
+                                              blur_sigma=blur_sigma)
+    mt_weights = mt_num / mt_den 
     # assign mtuple weights back to individual fields
     field_weights = torch.zeros((n, *shape)).to(device=mt_weights.device)
     for i, mtuple in enumerate(mtuples):
         for j in mtuple:
             field_weights[j] += mt_weights[i]
-    return field_weights
+    # rather than use m, prefer sum for sum precision
+    elements_per_subset = field_weights.sum(dim=0, keepdim=True)
+    field_weights = field_weights / elements_per_subset
+    return (self * field_weights.unsqueeze(-3)).sum(dim=0, keepdim=True)
