77d88db73d79b1a48beb1fb484f427761d5d5df6
Thomas Macrina <tmacrina@princeton.edu>
2020-08-12 21:40:23 +0000

    Add voting_with_variances to include prior weights

--- torchfields/fields.py
+++ torchfields/fields.py
@@ -924,6 +924,10 @@ class DisplacementField(torch.Tensor):
         return voting.get_vote_shape(self)
 
     @wraps(voting.vote)
+    def get_subset_size(self, subset_size=None):
+        return voting.get_subset_size(self, subset_size)
+
+    @wraps(voting.vote)
     def get_vote_subsets(self, subset_size=None):
         return voting.get_vote_subsets(self, subset_size)
 
@@ -934,6 +938,17 @@ class DisplacementField(torch.Tensor):
     @wraps(voting.vote)
     def vote(self, softmin_temp=1, blur_sigma=1, subset_size=None):
         return voting.vote(self, softmin_temp, blur_sigma, subset_size)
+
+    @wraps(voting.vote)
+    def get_vote_weights_with_variances(self, var, softmin_temp=1, blur_sigma=1, 
+                                        subset_size=None):
+        return voting.get_vote_weights_with_variances(self, var, softmin_temp, blur_sigma, 
+                                                      subset_size)
+
+    @wraps(voting.vote)
+    def vote_with_variances(self, var, softmin_temp=1, blur_sigma=1, subset_size=None):
+        return voting.vote_with_variances(self, var, softmin_temp, blur_sigma, subset_size)
+
 
 class set_identity_mapping_cache():
     """Context-manager that controls caching of identity_mapping() results.
